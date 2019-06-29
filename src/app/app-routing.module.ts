6b14108e7a851c2a4bc3a4e601383f709c9f00ea
Hemanth <vattikonda.h@zipgrid.com>
2019-06-29 16:29:13 +0530

    routing, pagenotfound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 
 const routes: Routes = [
-  {path:'', component: LoginComponent}
+  {path:'', component: LoginComponent},
+  {path: '**', component: PagenotfoundComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FormsModule } from '@angular/forms';
+
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent 
-    
-    
+    DashboardComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
