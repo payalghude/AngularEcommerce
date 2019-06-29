98edc50380b96002786dbbb4e606c3d470852870
payalghude <payal.ghude@gmail.com>
2019-06-29 17:45:15 +0530

    module changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
 
 
 const routes: Routes = [
   {path:'', component: LoginComponent},
+  {path:'dashboard' ,component:DashboardComponent},
+  {path:'AdminDashboard' ,component:AdminDashboardComponent},
+  {path:'UserDashboard' ,component:UserDashboardComponent},
+  {path:'UserRegistration' ,component:UserRegistrationComponent},
   {path: '**', component: PagenotfoundComponent}
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
