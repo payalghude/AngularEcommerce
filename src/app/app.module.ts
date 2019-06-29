4a919ae03a85db32eda2de19b68b57e48afff471
payalghude <payal.ghude@gmail.com>
2019-06-29 16:19:27 +0530

    login page, dashboard, material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent 
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  username: string ;
+  password: string ;
   constructor() { }
 
   ngOnInit() {
   }
+
+validateUser()
+{
+  console.log("method call") ; 
+  debugger;
+   if(this.username != null && this.password !=null)
+   {
+     if(this.username =="hemanth" && this.password=="123")
+     {
+       console.log("login sucessfully");
+     }
+   }
+}
 
 }
